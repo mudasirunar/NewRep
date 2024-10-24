a7ab9cac7197df32707b9caea8f5b5909e50cf88
mudasirunar <124304819+mudasirunar@users.noreply.github.com>
2024-10-24 21:30:02 +0500

    First Commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String[] args){
+		int num1 = 20;
+		int num2 = 40;
+		
+		System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
+		
+	}
+
+}
